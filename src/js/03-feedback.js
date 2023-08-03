377c9b422b8572a4c31c29561ac025886ecf0392
MaksymMoskalov <max.godnavi133@gmail.com>
2023-08-03 17:21:33 +0300

    mentor corrects

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,33 +5,34 @@ const FORM_KEY = 'feedback-form-state';
 formEl.addEventListener('input', _.throttle(inputMemory, 500));
 formEl.addEventListener('submit', onSubmit);
 
+let formState = {};
+
 setFromLocal();
 
 function onSubmit(e) {
   e.preventDefault();
 
-  console.log(localStorage.getItem(FORM_KEY));
+  console.log(formState);
+  formState = {};
   e.target.reset();
   localStorage.removeItem(FORM_KEY);
 }
 
 function inputMemory(e) {
-  const email = e.currentTarget.elements.email.value;
-  const message = e.currentTarget.elements.message.value.trim();
-  const objData = createObj(email, message);
-
-  localStorage.setItem(FORM_KEY, JSON.stringify(objData));
-}
-
-function createObj(email, message) {
-  return { email, message };
+  formState[e.target.name] = e.target.value.trim();
+  localStorage.setItem(FORM_KEY, JSON.stringify(formState));
 }
 
 function setFromLocal() {
-  const savedData = localStorage.getItem(FORM_KEY);
-  if (savedData) {
-    const parsed = JSON.parse(savedData);
-    formEl.elements.email.value = parsed.email;
-    formEl.elements.message.value = parsed.message;
+  try {
+    const savedData = localStorage.getItem(FORM_KEY);
+    if (!savedData) return;
+    formState = JSON.parse(savedData);
+
+    Object.entries(formState).forEach(([key, val]) => {
+      formEl.elements[key].value = val;
+    });
+  } catch (error) {
+    console.log(error.message);
   }
 }
